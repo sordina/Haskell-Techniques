22535501d8eeece4fda852587061a88db411be03
Lyndon Maydwell <maydwell@gmail.com>
2010-08-22 14:07:13 +0800

    Fixing output buffering at prompts.

--- Construction-Validation/main.hs
+++ Construction-Validation/main.hs
@@ -1,10 +1,14 @@
+import System.IO
 import Control.Monad (liftM)
 import Natural
 
 f :: Natural Integer -> Integer
 f = (* 10) . unNatural
 
+prompt :: String -> IO ()
+prompt s = putStr s >> hFlush stdout
+
 main = do
-  putStr "Enter a Natural number: "
+  prompt "Enter a Natural number: "
   n <- liftM read getLine
   putStrLn $ maybe "Supplied value is not Natural." (("Value * 10: "++).show.f) (mkNatural n)
