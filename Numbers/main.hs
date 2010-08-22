c2793c2e78a5cf8acb8e5b46d1f7dea5e67e4396
Lyndon Maydwell <maydwell@gmail.com>
2010-08-23 01:06:38 +0800

    About to make some structural changes.

--- Numbers/main.hs
+++ Numbers/main.hs
@@ -13,6 +13,31 @@ instance Fractional (Maybe Rational)
     fromRational = Just
     recip = (>>= \n -> if n == 0 then Nothing else Just (1/n))
 
+newtype Overflowable n = O {stagnant :: Maybe n}
+  deriving (Eq, Show, Ord)
+
+instance Bounded (Overflowable n)
+  where
+    minBound = undefined
+    maxBound = undefined
+
+instance Monad (Overflowable)
+  where
+    -- (Monad m) => m a -> (a -> m b) -> m b
+    --(O Nothing)  >>= f = O Nothing
+    --(O (Just a)) >>= f = O (Just (f a))
+    (>>=) = undefined
+    return n = O (Just n)
+    --return = undefined
+
+instance (Num n, Bounded n) => Num (Overflowable n)
+  where
+    (+)         = liftM2 (+)
+    (*)         = liftM2 (*)
+    abs         = liftM abs
+    signum      = liftM signum
+    fromInteger = O . Just . fromInteger
+
 a = Nothing
 b = Just 2
 c = Just 5
